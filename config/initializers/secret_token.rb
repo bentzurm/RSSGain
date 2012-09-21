7cf56bc405e75bcd80ace21bcdb629c9a07c768f
Moran Bentzur <moran@bentzur.com>
2012-09-21 23:05:40 +0300

    rails and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RSSGain::Application.config.secret_token = 'bdd6e63e3b772579379bff73e646ca415468ad6d5e6d81dc2a584180f3cf55a8d33a1546d3ea2501e8cfec14cd3b74c7f0a95b3c0914b64b7ff6f22bb03f89be'
