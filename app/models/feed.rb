4144da8b03953bf6145a4cb73089bab6c9435e97
Moran Bentzur <moran.bentzur@biodata.com>
2012-12-01 21:25:12 +0200

    add update function to feeds

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,8 +1,16 @@
 class Feed < ActiveRecord::Base
   belongs_to :user
 
-  attr_accessible :options, :title, :url, :user_id
+  attr_accessible :options, :title, :url, :user_id, :etag, :last_modified
 
   serialize :options, Hash
+
+  def self.update_feeds
+    Feed.all.each do |f|
+      feed = Feedzirra::Feed.fetch_and_parse(f.url)
+      puts ("updating #{f.title} - etag #{f.etag} => #{feed.etag}, last_modified #{f.last_modified} => #{feed.last_modified}")
+      f.update_attributes(:etag => feed.etag, :last_modified => feed.last_modified)
+    end
+  end
 
 end
